d23b4c079f755d9a870027258fb0f40881cda9d9
sudenazbuyukcukurlu <sudenazbuyukcukurlu@gmail.com>
2024-08-20 11:52:19 +0300

    Repository Interface yapıldı

---
+++ DataAccessLayer/Abstract/ICategoryDal.cs
@@ -0,0 +1,17 @@
+﻿using EntityLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface ICategoryDal
+    {
+        List<Category> List();
+        void Insert (Category P);
+        void Update (Category P);
+        void Delete (Category P);
+    }
+}
---
+++ DataAccessLayer/Concrete/Repositories/CategoryRepository.cs
@@ -0,0 +1,40 @@
+﻿using DataAccessLayer.Abstract;
+using EntityLayer;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete.Repositories
+{
+    internal class CategoryRepository : ICategoryDal
+    {
+        Context c= new Context();
+        DbSet<Category> _object;
+        public void Delete(Category P)
+        {
+            _object.Remove(P);
+            c.SaveChanges();
+        }
+
+        public void Insert(Category P)
+        {
+            _object.Add(P);
+            c.SaveChanges();
+
+
+        }
+
+        public List<Category> List()
+        {
+            return _object.ToList();
+        }
+
+        public void Update(Category P)
+        {
+            c.SaveChanges();
+        }
+    }
+}
