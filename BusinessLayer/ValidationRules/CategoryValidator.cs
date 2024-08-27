718fce361a0d7a57ba647037ca26b8becd3b5a64
sudenazbuyukcukurlu <sudenazbuyukcukurlu@gmail.com>
2024-08-27 16:04:29 +0300

    Fluent Validation

---
+++ BusinessLayer/ValidationRules/CategoryValidator.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class CategoryValidator:AbstractValidator<Category>
+    {
+        public CategoryValidator() 
+        {
+            RuleFor(x => x.CategoryName).NotEmpty().WithMessage("Kategori adını boş geçemezsiniz");
+            RuleFor(x => x.CategoryDescription).NotEmpty().WithMessage("açıkalmayı boş geçemezsiniz");
+            RuleFor(x => x.CategoryName).MinimumLength(3).WithMessage("En az 3 karakter girişi yapabilirsiniz");
+            RuleFor(x => x.CategoryName).MaximumLength(20).WithMessage("En fazla 20 karakter girişi yapabilirsiniz");
+        }
+    }
+}
